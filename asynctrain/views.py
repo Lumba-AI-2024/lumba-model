0ddeed95e2c5521dd4ffe4845b355f67baef7556
gbhisma <bhismagregorius@gmail.com>
2024-02-12 21:50:45 +0700

    Initial commit

---
+++ asynctrain/views.py
@@ -0,0 +1,68 @@
+import asyncio
+import json
+import os
+from django.http import JsonResponse
+import pandas as pd
+from ml_model.models.linear_regression import LumbaLinearRegression
+from ml_model.models.decision_tree import LumbaDecisionTreeClassifier
+import requests
+import joblib
+
+async def asynctrain(df, training_record, model_metadata):  
+
+    # update training record to 'in progress'
+    url = 'http://127.0.0.1:8000/modeling/updaterecord/'
+    json = {'id': training_record['id'], 'status':'in progress'}
+    record = requests.post(url, json=json)
+    print("training with record id "+ str(record.json()['id']) + " in progress")
+
+    # train model
+    if model_metadata['method'] == 'REGRESSION':
+      if model_metadata['algorithm'] == 'LINEAR':
+        LR = LumbaLinearRegression(df)
+        response = LR.train_model(train_column_name=model_metadata['feature'], target_column_name=model_metadata['target'])
+        model_metadata["metrics"] = "mean_absolute_error"
+        model_metadata["score"] = response["mean_absolute_error"]
+    if model_metadata['method'] == 'CLASSIFICATION':
+      if model_metadata['algorithm'] == 'DECISION_TREE':
+        DT = LumbaDecisionTreeClassifier(df)
+        response = DT.train_model(train_column_names=model_metadata['feature'].split(','), target_column_name=model_metadata['target'])
+        model_metadata["metrics"] = "accuracy_score"
+        model_metadata["score"] = response["accuracy_score"]
+    
+    # save model to pkl format
+    model_saved_name = f"{model_metadata['model_name']}.pkl"
+    joblib.dump(response['model'], model_saved_name)
+
+    # save model
+    url = 'http://127.0.0.1:8000/modeling/save/'
+    requests.post(url, data=model_metadata, files={'file': open(model_saved_name, 'rb')})
+    
+    # update training record to 'completed'
+    url = 'http://127.0.0.1:8000/modeling/updaterecord/'
+    json = {'id': training_record['id'], 'status':'completed'}
+    record = requests.post(url, json=json)
+    os.remove(model_saved_name)
+    print("training with record id "+ str(record.json()['id']) + " completed")
+
+# this function will return record in json 
+# {'id': 5, 'status': 'accepted'}
+async def async_train_endpoint(request):
+    try:
+      model_metadata = request.POST.dict()
+      file = request.FILES['file']
+    except:
+      return JsonResponse({'message': "input error"})
+    df = pd.read_csv(file)
+    
+    # create training record in main service db
+    url = 'http://127.0.0.1:8000/modeling/createrecord/'
+    json = {'status':'accepted'}
+    record = requests.post(url, json=json)
+    
+    training_record = {
+      'id' : record.json()['id'],
+      'status' : record.json()['status'],
+    }  
+    asyncio.gather(asynctrain(df, training_record, model_metadata))
+    return JsonResponse(training_record)
---
+++ ml_model/models/linear_regression.py
@@ -0,0 +1,64 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+from pandas.core.frame import DataFrame
+
+from typing import Any, Optional, Union, List
+
+class LumbaLinearRegression:
+    model: LinearRegression
+
+    def __init__(self, dataframe: DataFrame) -> None:
+        self.dataframe = dataframe
+
+    def train_model(self, train_column_name: Union[str, List[str]], target_column_name: str, train_size: float = 0.8) -> dict:
+        if self.dataframe[target_column_name].dtype not in ["int64", "float64"]:
+            return {
+                'error': 'Kolom yang boleh dipilih hanyalah kolom dengan data numerik saja. Silakan pilih kolom yang benar.'
+            }
+        
+        x = None
+        if type(train_column_name) == str:
+            if self.dataframe[train_column_name].dtype not in ["int64", "float64"]:
+                return {
+                    'error': 'Kolom yang boleh dipilih hanyalah kolom dengan data numerik saja. Silakan pilih kolom yang benar.'
+                }
+            x = self.dataframe[train_column_name].to_numpy().reshape(-1, 1)
+        
+        elif type(train_column_name) == list:
+            for col in train_column_name:
+                if self.dataframe[col].dtype not in ["int64", "float64"]:
+                    return {
+                        'error': 'Kolom yang boleh dipilih hanyalah kolom dengan data numerik saja. Silakan pilih kolom yang benar.'
+                    }
+
+            x = self.dataframe[train_column_name].to_numpy()
+
+        y = self.dataframe[target_column_name].to_numpy().reshape(-1, 1)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=train_size)
+        
+        lr = LinearRegression()
+        lr.fit(x_train, y_train)
+        
+        y_pred = lr.predict(x_test)
+        mae = mean_absolute_error(y_true=y_test, y_pred=y_pred)
+
+        self.model = lr
+
+        return {
+            'model': lr,
+            'mean_absolute_error': f'{mae:.4f}'
+        }
+
+    def get_model(self) -> Optional[LinearRegression]:
+        try:
+            return self.model
+        except AttributeError:
+            return None
+
+    def predict(self, data_target: Any) -> Any:
+        lr = self.get_model()
+        y_pred = lr.predict(data_target)
+
+        return y_pred
