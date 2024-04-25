86a0fec05f9f00f8375401bc57c675a66599df96
adella.rakha <adella.rakha@ui.ac.id>
2024-04-26 04:48:05 +0700

    ADD xgboost & dbscan

---
+++ ml_model/models/db_scan.py
@@ -0,0 +1,79 @@
+# https://towardsdatascience.com/k-means-clustering-algorithm-applications-evaluation-methods-and-drawbacks-aa03e644b48a
+
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import silhouette_score
+import optuna
+
+from pandas.core.frame import DataFrame
+
+from typing import Optional
+
+class LumbaDBScan:
+    model: DBSCAN
+
+    def __init__(self, dataframe: DataFrame) -> None:
+        self.dataframe = dataframe
+
+    def train_model(self) -> dict:
+        X = self.dataframe
+
+        # # check if the columns selected are valid for K-Means process
+        # for col in x.columns:
+        #     if x[col].dtype not in ["int64", "float64"]:
+        #         return {
+        #             'error': 'Kolom yang boleh dipilih hanyalah kolom dengan data numerik saja. Silakan pilih kolom yang benar atau gunakan encoding pada data categorical.'
+        #         }
+        def dbscan_objective(trial):
+            eps = trial.suggest_uniform('eps', 0.1, 2.0)
+            min_samples = trial.suggest_int('min_samples', 2, 20)
+            
+            # Valid metrics for KD-tree algorithm
+            valid_metrics_kd_tree = ['euclidean', 'manhattan']
+            metric = trial.suggest_categorical('metric', valid_metrics_kd_tree)
+            
+            algorithm = trial.suggest_categorical('algorithm', ['auto', 'ball_tree', 'kd_tree', 'brute'])
+            
+            dbscan = DBSCAN(eps=eps, min_samples=min_samples, metric=metric, algorithm=algorithm)
+            dbscan.fit(X)
+            
+            if len(set(dbscan.labels_)) > 1:  # Silhouette score requires at least 2 clusters
+                silhouette_avg = silhouette_score(X, dbscan.labels_)
+            else:
+                silhouette_avg = -1  # Return a negative score if only one cluster is found
+            
+            return silhouette_avg
+        
+                # Perform hyperparameter optimization for DBSCAN
+        dbscan_study = optuna.create_study(direction='maximize')
+        dbscan_study.optimize(dbscan_objective, n_trials=100)
+
+        # Get optimal hyperparameters from the study results
+        optimal_dbscan_eps = dbscan_study.best_params['eps']
+        optimal_dbscan_min_samples = dbscan_study.best_params['min_samples']
+        optimal_dbscan_metric = dbscan_study.best_params['metric']
+        optimal_dbscan_algorithm = dbscan_study.best_params['algorithm']
+
+        optimal_dbscan = DBSCAN(eps=optimal_dbscan_eps, min_samples=optimal_dbscan_min_samples, metric=optimal_dbscan_metric, algorithm=optimal_dbscan_algorithm)
+        optimal_dbscan.fit(X)
+
+        # predicted cluster labels
+        dbscan_cluster_labels = optimal_dbscan.labels_
+
+        # silhouette score
+        silhouette = silhouette_score(X, dbscan_cluster_labels)
+
+        self.model = optimal_dbscan
+
+        return {
+            'model': optimal_dbscan,
+            'labels_predicted': dbscan_cluster_labels,
+            'mean_absolute_error': f'{silhouette:.4f}'
+        }
+
+    def get_model(self) -> Optional[DBSCAN]:
+        try:
+            return self.model
+        except AttributeError:
+            return None
+
+    
---
+++ ml_model/models/xg_boost.py
@@ -0,0 +1,89 @@
+from xgboost import XGBRegressor
+from sklearn.model_selection import GridSearchCV,train_test_split, KFold
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+
+from pandas.core.frame import DataFrame
+
+from typing import Any, Optional, Union, List
+
+class LumbaXGBoost:
+    model: XGBRegressor
+
+    def __init__(self, dataframe: DataFrame) -> None:
+        self.dataframe = dataframe
+
+    def train_model(self, target_column_name: str) -> dict:
+        # if self.dataframe[target_column_name].dtype not in ["int64", "float64"]:
+        #     return {
+        #         'error': 'Kolom yang boleh dipilih hanyalah kolom dengan data numerik saja. Silakan pilih kolom yang benar.'
+        #     }
+        
+        # x = None
+        # if type(train_column_name) == str:
+        #     if self.dataframe[train_column_name].dtype not in ["int64", "float64"]:
+        #         return {
+        #             'error': 'Kolom yang boleh dipilih hanyalah kolom dengan data numerik saja. Silakan pilih kolom yang benar.'
+        #         }
+        #     x = self.dataframe[train_column_name].to_numpy().reshape(-1, 1)
+        
+        # elif type(train_column_name) == list:
+        #     for col in train_column_name:
+        #         if self.dataframe[col].dtype not in ["int64", "float64"]:
+        #             return {
+        #                 'error': 'Kolom yang boleh dipilih hanyalah kolom dengan data numerik saja. Silakan pilih kolom yang benar.'
+        #             }
+
+        #     x = self.dataframe[train_column_name].to_numpy()
+
+        # y = self.dataframe[target_column_name].to_numpy().reshape(-1, 1)
+        X = self.dataframe.drop(columns=[target_column_name])
+        y = self.dataframe[target_column_name]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        
+        xg = XGBRegressor()
+
+        # Define the grid search parameters
+        param_grid = {
+            'fit_intercept': [True, False],  # Whether to calculate the intercept for this model
+            'copy_X': [True, False],  # Whether to make a copy of X before fitting the model
+            'n_jobs': [-1],  # Number of jobs to run in parallel. -1 means using all processors.
+            'positive' : [True, False]
+        }
+
+        outer_cv = KFold(n_splits=5, shuffle=True, random_state=42)
+
+        # Perform grid search
+        grid = GridSearchCV(estimator=xg, param_grid=param_grid, cv=outer_cv)
+        grid_result = grid.fit(X_train, y_train)  # Assuming X and y are your feature matrix and target vector
+        
+        best_hyperparams = grid_result.best_params_
+
+        # Evaluate the best model and count accuracy
+        best_model = grid_result.best_estimator_
+        y_pred = best_model.predict(X_test)
+        r2 = r2_score(y_true=y_test, y_pred=y_pred)
+        mae = mean_absolute_error(y_true=y_test, y_pred=y_pred)
+        mse = mean_squared_error(y_true=y_test, y_pred=y_pred)
+
+        self.model = xg
+
+        return {
+            'model': xg,
+            'best_hyperparams': best_hyperparams,
+            'mean_absolute_error': f'{mae:.4f}',
+            'mean_squared_error': f'{mse:.4f}',
+            'r2_score': f'{r2:.4f}'
+        }
+    
+    def get_model(self) -> Optional[XGBRegressor]:
+        try:
+            return self.model
+        except AttributeError:
+            return None
+
+    # def predict(self, data_target: Any) -> Any:
+    #     lr = self.get_model()
+    #     y_pred = lr.predict(data_target)
+
+    #     return y_pred
