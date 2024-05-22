67c907a245f5cada80d58dc66896897316315718
adella.rakha <adella.rakha@ui.ac.id>
2024-05-22 15:01:06 +0700

    fix lumba-model

--- asynctrain/views.py
+++ asynctrain/views.py
@@ -16,9 +16,18 @@ from ml_model.models.kmeans import LumbaKMeans
 from ml_model.models.db_scan import LumbaDBScan
 import requests
 import joblib
+import shap
+
+def calculate_shap_values(best_model, X_test):
+		explainer = shap.TreeExplainer(best_model)
+		shap_values = explainer.shap_values(X_test)
+		
+		# Convert SHAP values to a format that can be returned as JSON
+		shap_values_json = [shap_values[i].tolist() for i in range(len(shap_values))]
+		
+		return shap_values_json
 
 async def asynctrain(df, training_record, model_metadata):
-
 	# update training record to 'in progress'
 	# TODO: commented out for dev
 	# url = 'http://127.0.0.1:8000/modeling/updaterecord/'
@@ -40,6 +49,7 @@ async def asynctrain(df, training_record, model_metadata):
 			response = DTR.train_model(target_column_name=model_metadata['target'])
 			model_metadata["metrics"] = "r2_score"
 			model_metadata["score"] = response["r2_score"]
+			model_metadata["model"] = response["model"]
 		if model_metadata['algorithm'] == 'RANDOM_FOREST':
 			RFR = LumbaRandomForestRegressor(df)
 			response = RFR.train_model(target_column_name=model_metadata['target'])
@@ -62,6 +72,7 @@ async def asynctrain(df, training_record, model_metadata):
 			response = DTC.train_model(target_column_name=model_metadata['target'])
 			model_metadata["metrics"] = "accuracy_score"
 			model_metadata["score"] = response["accuracy_score"]
+			model_metadata["model"] = response["model"]
 		if model_metadata['algorithm'] == 'NEURAL_NETWORK':
 			NNC = LumbaNeuralNetworkClassification(df)
 			response = NNC.train_model(target_column_name=model_metadata['target'])
@@ -92,12 +103,15 @@ async def asynctrain(df, training_record, model_metadata):
 			model_metadata["score"] = response["silhouette_score"]
 			# model_metadata["labels"] = response["labels_predicted"]
 
+	shap_values = calculate_shap_values(model_metadata["model"], df.drop(columns=[model_metadata['target']]))
 	# save model to pkl format
 	model_saved_name = f"{model_metadata['model_name']}.pkl"
 	joblib.dump(response['model'], model_saved_name)
 
 	# save the pkl in the model_metadata
 	model_metadata['model'] = model_saved_name
+ 
+	model_metadata['shap_values'] = shap_values
 	
 
 	# save model
