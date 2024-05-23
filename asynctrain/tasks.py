c861533ff7e71d48ae7db340231c4532e719b2c0
adella.rakha <adella.rakha@ui.ac.id>
2024-05-24 00:15:33 +0700

    fix shapley

--- asynctrain/tasks.py
+++ asynctrain/tasks.py
@@ -2,6 +2,9 @@ import joblib
 import pandas
 import requests
 import shap
+from io import BytesIO
+import matplotlib.pyplot as plt
+import base64
 from celery import shared_task
 from minio import Minio
 
@@ -17,14 +20,30 @@ from ml_model.models.neural_network_classification import LumbaNeuralNetworkClas
 from ml_model.models.kmeans import LumbaKMeans
 from ml_model.models.db_scan import LumbaDBScan
 
-def calculate_shap_values(best_model, X_test):
-		explainer = shap.TreeExplainer(best_model)
-		shap_values = explainer.shap_values(X_test)
-		
-		# Convert SHAP values to a format that can be returned as JSON
-		shap_values_json = [shap_values[i].tolist() for i in range(len(shap_values))]
-		
-		return shap_values_json
+def calculate_shap_values(best_model, X_test, model_type):
+    if model_type == "classification":
+        explainer = shap.Explainer(best_model)
+        shap_values = explainer.shap_values(X_test)
+    elif model_type == "regression":
+        explainer = shap.Explainer(best_model, X_test)
+        shap_values = explainer.shap_values(X_test)
+    elif model_type == "neural_network":
+        explainer = shap.KernelExplainer(best_model.predict, X_test)
+        shap_values = explainer.shap_values(X_test)
+    else:
+        raise ValueError("Unsupported model type")
+
+    # Generate SHAP summary plot
+    plt.figure()
+    shap.summary_plot(shap_values, X_test, plot_type="bar", show=False)
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    img_str = base64.b64encode(buf.read()).decode('utf-8')
+    plt.close()
+
+    return img_str
+
 @shared_task
 def asynctrain(model_metadata):
     url = 'http://127.0.0.1:8000/modeling/'
@@ -46,6 +65,7 @@ def asynctrain(model_metadata):
 
     # train model
     print("inii",model_metadata)
+    model_type = ""
     if model_metadata['method'] == 'REGRESSION':
         if model_metadata['algorithm'] == 'LINEAR':
             LR = LumbaLinearRegression(df)
@@ -53,6 +73,7 @@ def asynctrain(model_metadata):
             model_metadata["metrics"] = "r2_score"
             model_metadata["score"] = response["r2_score"]
             model_metadata["model"] = response["model"]
+            model_type = "regression"
         if model_metadata['algorithm'] == 'DECISION_TREE':
             DTR = LumbaDecisionTreeRegressor(df)
             print("masukk")
@@ -60,24 +81,28 @@ def asynctrain(model_metadata):
             model_metadata["metrics"] = "r2_score"
             model_metadata["score"] = response["r2_score"]
             model_metadata["model"] = response["model"]
+            model_type = "regression"
         if model_metadata['algorithm'] == 'RANDOM_FOREST':
             RFR = LumbaRandomForestRegressor(df)
             response = RFR.train_model(target_column_name=model_metadata['target'])
             model_metadata["metrics"] = "r2_score"
             model_metadata["score"] = response["r2_score"]
             model_metadata["model"] = response["model"]
+            model_type = "regression"
         if model_metadata['algorithm'] == 'NEURAL_NETWORK':
             NNR = LumbaNeuralNetworkRegression(df)
             response = NNR.train_model(target_column_name=model_metadata['target'])
             model_metadata["metrics"] = "r2_score"
             model_metadata["score"] = response["r2_score"]	
             model_metadata["model"] = response["model"]	
+            model_type = "neural_network"
         if model_metadata['algorithm'] == 'XG_BOOST':
             XBR = LumbaXGBoostRegressor(df)
             response = XBR.train_model(target_column_name=model_metadata['target'])
             model_metadata["metrics"] = "r2_score"
             model_metadata["score"] = response["r2_score"]	
             model_metadata["model"] = response["model"]	
+            model_type = "regression"
             
     if model_metadata['method'] == 'CLASSIFICATION':
         if model_metadata['algorithm'] == 'DECISION_TREE':
@@ -86,24 +111,28 @@ def asynctrain(model_metadata):
             model_metadata["metrics"] = "accuracy_score"
             model_metadata["score"] = response["accuracy_score"]
             model_metadata["model"] = response["model"]
+            model_type = "classification"
         if model_metadata['algorithm'] == 'NEURAL_NETWORK':
             NNC = LumbaNeuralNetworkClassification(df)
             response = NNC.train_model(target_column_name=model_metadata['target'])
             model_metadata["metrics"] = "accuracy_score"
             model_metadata["score"] = response["accuracy_score"]
             model_metadata["model"] = response["model"]
+            model_type = "neural_network"
         if model_metadata['algorithm'] == 'RANDOM_FOREST':
             RFC = LumbaRandomForestClassifier(df)
             response = RFC.train_model(target_column_name=model_metadata['target'])
             model_metadata["metrics"] = "accuracy_score"
             model_metadata["score"] = response["accuracy_score"]
             model_metadata["model"] = response["model"]
+            model_type = "neural_network"
         if model_metadata['algorithm'] == 'XG_BOOST':
             XGC = LumbaXGBoostClassifier(df)
             response = XGC.train_model(target_column_name=model_metadata['target'])
             model_metadata["metrics"] = "accuracy_score"
             model_metadata["score"] = response["accuracy_score"]
             model_metadata["model"] = response["model"]
+            model_type = "neural_network"
 
     if model_metadata['method'] == 'CLUSTER':
         if model_metadata['algorithm'] == 'KMEANS':
@@ -119,7 +148,7 @@ def asynctrain(model_metadata):
             model_metadata["score"] = response["silhouette_score"]
             model_metadata["labels"] = response["labels_predicted"]
 
-    shap_values = calculate_shap_values(model_metadata["model"], df.drop(columns=[model_metadata['target']]))
+    shap_values = calculate_shap_values(model_metadata["model"], df.drop(columns=[model_metadata['target']]), model_type)
     model_metadata['shap_values'] = shap_values
     # save model to pkl format
     model_saved_name = f"{model_metadata['modelname']}.pkl"
